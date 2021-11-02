41050b1abaa5e80db445bfb633a6025b5a71144c
Vlasis <vlasism@gmail.com>
2021-11-02 22:52:05 +0200

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import navBar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+const Navbar = () => {
+  return <div>Vlasis Mentzelopoulos</div>;
+};
+
+export default Navbar;
