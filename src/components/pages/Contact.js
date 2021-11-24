1fe9f24011232b988489a6179edeb6d06047b2b4
Vlasis <vlasism@gmail.com>
2021-11-24 17:14:30 +0200

    fix contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,11 +1,65 @@
-import React from 'react';
+import React from "react";
+import { Container, Row, Form, Button } from "react-bootstrap";
 
 const Contact = () => {
   return (
     <>
-      <h1 style={{ paddingTop: '400px', textAlign: 'center' }}>
-        TODO - Contact Page
-      </h1>
+      <Container fluid className="contact-container">
+        <h2 className="contact-title">Contact me, I'd love to hear from you</h2>
+        <div className="contactForm">
+          <Form>
+            <Form.Group className="form-group">
+              <Form.Label htmlFor="name">Name</Form.Label>
+              <Form.Control
+                className="input-form"
+                name="name"
+                type="text"
+                placeholder="Your name"
+                // ref={nameRef}
+              />
+            </Form.Group>
+
+            <Form.Group className="form-group">
+              <Form.Label htmlFor="email">Email</Form.Label>
+              <Form.Control
+                className="input-form"
+                name="email"
+                type="email"
+                placeholder="Your Email"
+                // ref={emailRef}
+              />
+            </Form.Group>
+
+            <Form.Group className="form-group">
+              <Form.Label htmlFor="message">Your message</Form.Label>
+              <Form.Control
+                className="input-form"
+                name="message"
+                as="textarea"
+                rows="3"
+                placeholder="Your message"
+                // ref={messageRef}
+              />
+            </Form.Group>
+
+            <Button
+              // onClick={submitMessage}
+              className="form-button"
+              size="lg"
+              variant="primary"
+              type="submit"
+            >
+              Send
+            </Button>
+
+            {/*{state.sent ? (*/}
+            {/*  <strong>Email Sent</strong>*/}
+            {/*) : (*/}
+            {/*  <strong>Email Not Sent</strong>*/}
+            {/*)}*/}
+          </Form>
+        </div>
+      </Container>
     </>
   );
 };
