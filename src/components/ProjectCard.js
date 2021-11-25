40bbff0b460215e5dc6b48d7ac256e9a2fe33178
Vlasis <vlasism@gmail.com>
2021-11-25 13:11:04 +0200

    finish projects page

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { BiLinkExternal } from "react-icons/bi";
 
 function ProjectCard(props) {
   return (
-    <Card className="projects-card">
+    <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>{props.description}</Card.Text>
+        <Button variant="primary" href={props.link} target="_blank">
+          <BiLinkExternal /> View Project
+        </Button>
       </Card.Body>
     </Card>
   );
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,6 +3,9 @@ import { Col, Container, Row } from "react-bootstrap";
 import ProjectCard from "../ProjectCard";
 
 import grocery from "../../assets/grocery.JPG";
+import menu from "../../assets/menu.JPG";
+import cocktail from "../../assets/cocktail.JPG";
+import quiz from "../../assets/quiz.JPG";
 
 const Projects = () => {
   return (
@@ -10,33 +13,38 @@ const Projects = () => {
       <h1 className="projects-heading">
         Some Of My<strong> Projects</strong>
       </h1>
+
       <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-        <Col md={4}>
+        <Col xl={3} lg={4} md={6} className="project-card">
           <ProjectCard
             imgPath={grocery}
             title="Grocery"
             description="A simple grocery list keeper made with React."
+            link="https://grocery-bud-react-vlasis.netlify.app/"
           />
         </Col>
-        <Col md={4}>
+        <Col xl={3} lg={4} md={6} className="project-card">
           <ProjectCard
-            imgPath={grocery}
-            title="Grocery"
-            description="A simple grocery list keeper made with React."
+            imgPath={menu}
+            title="Menu"
+            description="Lorem ipsum dolor sit amet, sdfgffff consectetur adipiscing elit, sed do eiusmod tempor"
+            link="https://reactmenuproject.netlify.app/"
           />
         </Col>
-        <Col md={4}>
+        <Col xl={3} lg={4} md={6} className="project-card">
           <ProjectCard
-            imgPath={grocery}
-            title="Grocery"
-            description="A simple grocery list keeper made with React."
+            imgPath={cocktail}
+            title="Cocktail"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed "
+            link="https://vlasis-cocktail-db.netlify.app/"
           />
         </Col>
-        <Col md={4}>
+        <Col xl={3} lg={4} md={6} className="project-card">
           <ProjectCard
-            imgPath={grocery}
-            title="Grocery"
-            description="A simple grocery list keeper made with React."
+            imgPath={quiz}
+            title="Quiz"
+            description="A simple grocery list keeper made with React.Lorem ipsum dolor sit amet, sdfgffff consectetur adipiscing elit,Lorem ipsum dolor sit amet, sdfgffff con"
+            link="https://vlasis-quiz.netlify.app/"
           />
         </Col>
       </Row>
