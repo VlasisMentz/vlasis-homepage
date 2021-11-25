7ff733379603bb5572cd5ea7dab2cb6c6231b469
Vlasis <vlasism@gmail.com>
2021-11-25 17:27:30 +0200

    fix contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,49 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Form, Button } from "react-bootstrap";
+import { init } from "emailjs-com";
+init("user_rpUp0bK7uNz6KClBE30pU");
+// import Swal from "sweetalert2";
+
+// const SERVICE_ID = "service_m72leld";
+// const TEMPLATE_ID = "template_d3e332e";
+// const USER_ID = "user_rpUp0bK7uNz6KClBE30pU";
 
 const Contact = () => {
+  const initialFormData = Object.freeze({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [formData, updateFormData] = useState(initialFormData);
+
+  const handleChange = (e) => {
+    updateFormData({ ...formData, [e.target.name]: e.target.value.trim() });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Message Sent Successfully");
+    const templateId = "template_d3e332e";
+    const serviceID = "service_m72leld";
+    sendFeedback(serviceID, templateId, {
+      from_name: formData.name,
+      message: formData.message,
+      email: formData.email,
+    });
+  };
+  const sendFeedback = (serviceID, templateId, variables) => {
+    window.emailjs
+      .send(serviceID, templateId, variables)
+      .then((res) => {
+        console.log("email sent successfully");
+      })
+      .catch((err) => console.error("There has been an error.", err));
+  };
+  // const handleOnSubmit = (e) => {
+  //   console.log(e.target.value);
+  //   e.preventDefault();
+  //   emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then(
+  //     (result) => {
+  //       console.log(e.target);
+  //       Swal.fire({ icon: "success", title: "Message Sent Succesfully" });
+  //     },
+  //     (error) => {
+  //       Swal.fire({
+  //         icon: "error",
+  //         title: "Ooops, something went wrong",
+  //         text: error.text,
+  //       });
+  //     }
+  //   );
+  //   e.target.reset();
+  // };
+
   return (
     <>
       <Container fluid className="contact-container">
-        <h2 className="contact-title">Contact me, I'd love to hear from you</h2>
+        <h2 className="contact-title">Contact Me</h2>
         <div className="contactForm">
           <Form>
-            <Form.Group className="form-group">
-              <Form.Label htmlFor="name">Name</Form.Label>
+            <Form.Group className="form-group" controlId="formGridName">
+              <Form.Label>Name</Form.Label>
               <Form.Control
-                className="input-form"
+                onChange={handleChange}
                 name="name"
                 type="text"
                 placeholder="Your name"
-                // ref={nameRef}
               />
             </Form.Group>
 
-            <Form.Group className="form-group">
-              <Form.Label htmlFor="email">Email</Form.Label>
+            <Form.Group className="form-group" controlId="formGridEmail">
+              <Form.Label>Email</Form.Label>
               <Form.Control
-                className="input-form"
+                onChange={handleChange}
                 name="email"
                 type="email"
                 placeholder="Your Email"
-                // ref={emailRef}
               />
             </Form.Group>
-
-            <Form.Group className="form-group">
-              <Form.Label htmlFor="message">Your message</Form.Label>
+            <Form.Group className="form-group" controlId="formGridMessage">
+              <Form.Label>Your message</Form.Label>
               <Form.Control
-                className="input-form"
+                onChange={handleChange}
                 name="message"
                 as="textarea"
                 rows="3"
                 placeholder="Your message"
-                // ref={messageRef}
               />
             </Form.Group>
-
             <Button
-              // onClick={submitMessage}
+              onClick={handleSubmit}
               className="form-button"
               size="lg"
               variant="primary"
@@ -51,12 +101,6 @@ const Contact = () => {
             >
               Send
             </Button>
-
-            {/*{state.sent ? (*/}
-            {/*  <strong>Email Sent</strong>*/}
-            {/*) : (*/}
-            {/*  <strong>Email Not Sent</strong>*/}
-            {/*)}*/}
           </Form>
         </div>
       </Container>
